95e3e4c409fbe7de64a820568fc501ec2f0dcefe
Asilvazavala <asilvazavala@gmail.com>
2023-08-23 11:18:12 -0600

    modified validation

--- PF_Client/src/views/Checkout/FormCheckout/Validation.jsx
+++ PF_Client/src/views/Checkout/FormCheckout/Validation.jsx
@@ -1,6 +1,4 @@
 export const Validation = (input) => {
-  // Debe iniciar con mayúscula, puede llevar espacios
-  const nameRegExp = /^[A-Z\s].*/
   // El valor debe estar entre 1 y 10000
   const numRegExp = /^([1-9]|[0-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|10000)$/; 
   
@@ -16,16 +14,12 @@ export const Validation = (input) => {
   // Validation Input Ciudad
   if (!input.ciudad) {
     err.ciudad = 'Ingresa una ciudad válida';
-    } else if (input.ciudad.length < 5) {
-        err.ciudad = 'La ciudad debe tener al menos 5 carácteres';
-      }
+  }
 
   // Validation Input calle
   if (!input.calle.length) {
     err.calle = 'Ingresa una calle válida';
-  } else if (input.calle.length < 5) {
-      err.calle = 'La calle debe tener al menos 5 carácteres';
-    } 
+  } 
 
   // Validation Input numero_exterior 
   if (!input.numero_exterior) {
